fda6f397e932be50a2d40dc3a255207d3912674e
Matt <mattmaliaros@gmail.com>
2019-12-10 18:01:18 -0500

    heroku

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'angular-crash-todolist';
+  name: string = "Matt";
+
+  constructor() {
+    this.changeName("John");
+  }
+  changeName(name: string): void {
+    this.name = name;
+  }
 }
